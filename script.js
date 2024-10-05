25bd8c9ed6a11cc4e73d6caa6596201d18f55e4d
Bilol5577 <bilolbektursunov079@gmail.com>
2024-10-05 20:20:57 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,44 @@
+let myNameIsm = prompt("ismingizni kiriting")
+console.log(myNameIsm)
+
+let yurfamiliya = prompt("Familiyangizni kiriting")
+console.log(yurfamiliya)
+
+let age = prompt("Yoshingiz nechida")
+console.log(age)
+
+ let manzil = prompt("manzilingizni kiriting")
+ console.log(manzil)
+
+let fan = prompt("Yahshi korgan faningiz nima")
+console.log(fan)
+
+// alert("Hello world")
+// confirm("Bu a harifimi?")
+// prompt("Ismingiz nima")
+// console.log();
+// let box = "apple"
+// alert('box')
+
+// console.log('box');
+
+// rest = 123
+// console.log(rest)
+
+// let myName = "Bobur"
+// console.log(myName);
+
+// let familiya = "Bohodirov"
+// console.log(familiya)
+
+// let ota_ismi = "Bahtiyor"
+// console.log(ota_ismi)
+
+// let ona_ismi = "Dildora"
+// console.log(ona_ismi)
+
+// let opa_uka_ismi = "Ibrohim"
+// console.log(opa_uka_ismi)
+
+// let age = "2011"
+// console.log(age)
